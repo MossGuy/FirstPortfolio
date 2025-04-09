0e49bffbe447f343ae18429dfb826f6b31780bba
noname <>
2025-04-09 12:24:17 +0200

    talenkennis bijgewerkt

--- javascript_functies/index.js
+++ javascript_functies/index.js
@@ -24,34 +24,44 @@ const html_kennis = `
 const css_kennis = `
     Ik kan goed onderscheid maken in het gebruik van classes en selectors. Verder heb ik kennis met Flexbox, Grids en CSS variabelen.
 
-    Ik maak gebruik van %, vh, vw en rem en px. Mijn keuze over wanneer ik een van deze eenheden gebruik heeft nog geen methode.
+    Ik maak gebruik van %, vh, vw en rem en px.
+
+    Media queries kan ik toepassen aan de hand van codevoorbeelden voor de syntax. 
 `;
 const bootstrap_kennis = `
-    Ik kan prima overweg met de basis functionaliteiten van Bootstrap, in combinatie met mijn eigen CSS.
+    Het is even een tijdje geleden zinds ik echt heb gewerkt met bootstrap, een groot gedeelte waar ik vroeger convertabel mee kon werken ben ik ongetwijfeld verleerd. 
+    Tegenwoordig pas ik Bootstrap vaak toe voor de icons. 
 
     Geafanceerde functies zoals toasts, accordions en navbar collapse kan ik alleen aan de hand van code voorbeelden.
 `;
 const php_kennis = `
-    Ik heb al veel ervaring met PHP, in samenwerking met HTML en SQL via PDO. Ik kan goed werken met functies, sessions en arrays.
+    PHP is momenteel mijn lievelings programmeertaal!
+
+    Ik heb al veel ervaring met PHP, in samenwerking met HTML en SQL via PDO. Ik kan goed werken met functies, sessions en associative arrays.
+    
     Het maken van een DBConfig is nog wat lastig te onthouden haha.
 `;
 const javascript_kennis = `
-    Ik heb nog maar een basis kennis met Javascript. Ik kan werken met gewone en async functies,  eventlisteners, classes en API's.
+    Ik heb een basis kennis met Javascript. Ik kan werken met gewone en async functies, eventlisteners en API's.
     Dit leuke element is ook gemaakt Javascript :)
 `;
 const mysql_Kennis = `
     Ik kan basis databases maken en onderhouden aan de hand van code voorbeelden voor de syntax.
 
-    Daarnaast heb ik kennig over geavanceerde select statements,
-    maar met een grote hoeveelheid functies en weinig oefening is ook dit lastig te onthouden.
+    Daarnaast heb ik kennis over geavanceerde select statements,
+    maar met een grote hoeveelheid functies en weinig oefening is dit lastig te onthouden.
     
     Laten we het maar niet hebben over joins... thx!
 `;
 const CSharp_kennis = `
-    C# is voor mij nog wat lastig. Ik heb een basis kennis over classes, methods, constructors en erving.
-    
-    Om complexe programma's te maken die veel classes bevatten moet ik altijd even mijn weg vinden.
-    Dit zorgd ervoor dat ik er langer over doe om C# programma's te bouwen.
+    C# is momenteel mijn mist favoriete programmeertaal. 
+
+    Ook met C# heb ik vanuit school en stage kennis opgebouwd, maar het leren van deze taal heb ik het meeste tijd voor moeten besteden in verhouding met andere talen die ik heb geleerd.
+
+    een paar dingen waar ik ervaring mee heb zijn methods, constructors, erving en verschillende data types.
+
+    Ook heb ik een tijdje met ASP.NET gewerkt, maar ik dat framework echt helemaal niks.
+    Ik vind de bestandsstructuur super groot, en ik werk toch liever met XAMPP en Visual Studio Code :p
 `;
 
 prog_talen.addEventListener('change', () => {
